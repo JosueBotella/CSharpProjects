d50e6e86f89f070d141a65bfe9299773cf56016e
josuebotella@gmail.com <josuebotella@gmail.com>
2015-02-23 19:21:19 +0100

    Primera vez

---
+++ AltaMuestrasUbicaciones/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace AltaMuestrasUbicaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.productoLb = new System.Windows.Forms.Label();
+            this.productoeantxbox = new System.Windows.Forms.TextBox();
+            this.esProductolb = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listaResultados = new System.Windows.Forms.ListView();
+            this.btn_aplicar = new System.Windows.Forms.Button();
+            this.codigoarticulocol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.descripcioncol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.codigoalternativocol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tipomuestracol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ubicacioncol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sagelb = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // productoLb
+            // 
+            this.productoLb.AutoSize = true;
+            this.productoLb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.productoLb.ForeColor = System.Drawing.Color.Red;
+            this.productoLb.Location = new System.Drawing.Point(12, 28);
+            this.productoLb.Name = "productoLb";
+            this.productoLb.Size = new System.Drawing.Size(131, 17);
+            this.productoLb.TabIndex = 0;
+            this.productoLb.Text = "PRODUCTO EAN";
+            // 
+            // productoeantxbox
+            // 
+            this.productoeantxbox.Location = new System.Drawing.Point(163, 28);
+            this.productoeantxbox.Name = "productoeantxbox";
+            this.productoeantxbox.Size = new System.Drawing.Size(263, 20);
+            this.productoeantxbox.TabIndex = 1;
+            this.productoeantxbox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.productoeantxbox_KeyDown);
+            // 
+            // esProductolb
+            // 
+            this.esProductolb.AutoSize = true;
+            this.esProductolb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.esProductolb.Location = new System.Drawing.Point(670, 28);
+            this.esProductolb.Name = "esProductolb";
+            this.esProductolb.Size = new System.Drawing.Size(61, 20);
+            this.esProductolb.TabIndex = 2;
+            this.esProductolb.Text = "NONE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(513, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Último Tickado:";
+            // 
+            // listaResultados
+            // 
+            this.listaResultados.BackColor = System.Drawing.SystemColors.Info;
+            this.listaResultados.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.codigoarticulocol,
+            this.descripcioncol,
+            this.codigoalternativocol,
+            this.tipomuestracol,
+            this.ubicacioncol});
+            this.listaResultados.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listaResultados.ForeColor = System.Drawing.Color.Red;
+            this.listaResultados.FullRowSelect = true;
+            this.listaResultados.GridLines = true;
+            this.listaResultados.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listaResultados.Location = new System.Drawing.Point(15, 78);
+            this.listaResultados.MultiSelect = false;
+            this.listaResultados.Name = "listaResultados";
+            this.listaResultados.Size = new System.Drawing.Size(1069, 116);
+            this.listaResultados.TabIndex = 4;
+            this.listaResultados.UseCompatibleStateImageBehavior = false;
+            this.listaResultados.View = System.Windows.Forms.View.Details;
+            // 
+            // btn_aplicar
+            // 
+            this.btn_aplicar.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_aplicar.ForeColor = System.Drawing.Color.Red;
+            this.btn_aplicar.Location = new System.Drawing.Point(797, 233);
+            this.btn_aplicar.Name = "btn_aplicar";
+            this.btn_aplicar.Size = new System.Drawing.Size(287, 73);
+            this.btn_aplicar.TabIndex = 5;
+            this.btn_aplicar.Text = "APLICAR UBICACION";
+            this.btn_aplicar.UseVisualStyleBackColor = true;
+            this.btn_aplicar.Click += new System.EventHandler(this.btn_aplicar_Click);
+            // 
+            // codigoarticulocol
+            // 
+            this.codigoarticulocol.Text = "Codigo Articulo";
+            this.codigoarticulocol.Width = 120;
+            // 
+            // descripcioncol
+            // 
+            this.descripcioncol.Text = "Descripcion Articulo ";
+            this.descripcioncol.Width = 400;
+            // 
+            // codigoalternativocol
+            // 
+            this.codigoalternativocol.Text = "Codigo Alternativo";
+            this.codigoalternativocol.Width = 150;
+            // 
+            // tipomuestracol
+            // 
+            this.tipomuestracol.Text = "Tipo Muestra";
+            this.tipomuestracol.Width = 120;
+            // 
+            // ubicacioncol
+            // 
+            this.ubicacioncol.Text = "Ubicacion";
+            this.ubicacioncol.Width = 80;
+            // 
+            // sagelb
+            // 
+            this.sagelb.AutoSize = true;
+            this.sagelb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sagelb.Location = new System.Drawing.Point(834, 28);
+            this.sagelb.Name = "sagelb";
+            this.sagelb.Size = new System.Drawing.Size(51, 20);
+            this.sagelb.TabIndex = 6;
+            this.sagelb.Text = "Sage";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1234, 729);
+            this.Controls.Add(this.sagelb);
+            this.Controls.Add(this.btn_aplicar);
+            this.Controls.Add(this.listaResultados);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.esProductolb);
+            this.Controls.Add(this.productoeantxbox);
+            this.Controls.Add(this.productoLb);
+            this.Name = "Form1";
+            this.Text = "Altas Muestras - Ubicacion";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label productoLb;
+        private System.Windows.Forms.TextBox productoeantxbox;
+        private System.Windows.Forms.Label esProductolb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listaResultados;
+        private System.Windows.Forms.Button btn_aplicar;
+        private System.Windows.Forms.ColumnHeader codigoarticulocol;
+        private System.Windows.Forms.ColumnHeader descripcioncol;
+        private System.Windows.Forms.ColumnHeader codigoalternativocol;
+        private System.Windows.Forms.ColumnHeader tipomuestracol;
+        private System.Windows.Forms.ColumnHeader ubicacioncol;
+        private System.Windows.Forms.Label sagelb;
+    }
+}
+
---
+++ AltaMuestrasUbicaciones/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Timers;
+
+namespace AltaMuestrasUbicaciones
+{
+    public partial class Form1 : Form
+    {
+        ListViewItem elementoListView;
+        string sageID = "";
+        string muestraID = "";
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+                
+        }
+        private void productoeantxbox_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            string barcode = productoeantxbox.Text;
+            if (e.KeyCode == System.Windows.Forms.Keys.Return)
+            {
+                //Comprobamos si el barcode tickado longitud correcta. 
+                if (productoeantxbox.Text.Length > 0 && productoeantxbox.Text.Length < 14)
+                {
+                    listaResultados.Items.Clear();
+                    string codeEan = productoeantxbox.Text;
+                    if (codeEan.Length == 12) codeEan = "0" + codeEan;
+                    
+                    //Tickamos producto e asignamos a variable local su valor                    
+                    esProductolb.Text = productoeantxbox.Text;                    
+                    existeArticulo(codeEan);
+                    productoeantxbox.Text = "";
+                    btn_aplicar.Enabled = true;
+                }
+                else { MessageBox.Show("PRODUCTO INCORRECTO, CODIGO EAN DEMASIADO LARGO."); }
+            }
+        }
+
+        private void btn_aplicar_Click(object sender, EventArgs e)
+        {
+            DialogResult result1 = MessageBox.Show("Quieres asignar la ubicación a la muestra?", "Si o No?", MessageBoxButtons.YesNo);
+            if (result1 == DialogResult.Yes)
+            {
+                MessageBox.Show("Elemento con sage :"+sageID+" y ubicacion "+muestraID);
+                productoUbicado();
+                sageID = "";
+                muestraID = "";
+                //MessageBox.Show("Elemento con ubicación:" + listaResultados.SelectedItems["CodigoArticulo"]);
+            }
+            else { MessageBox.Show("Elemento con ubicación:"); }
+        }
+
+        private void existeArticulo(string barcode)
+        {           
+            try
+            {
+                
+                SqlConnection conectorSQL = new SqlConnection("user id=sa;password=Admin123admin;server=SRVSQL\\LOGICCLASS;database=lc_perfumesvalencia;connection timeout=30");
+                string consulta = "SELECT  CodigoArticulo, DescripcionArticulo + Descripcion2Articulo AS descripcion, CodigoAlternativo, B_EsVapo, B_CodigoMarca, B_Muestra ";
+                consulta += " FROM  Articulos WHERE  (CodigoAlternativo = '"+barcode+"')";
+                conectorSQL.Open();
+                SqlCommand comando = new SqlCommand(consulta, conectorSQL);
+                SqlDataReader datos = comando.ExecuteReader();
+                
+                if (datos.HasRows) {
+                    string[] elementosFila = new string[5];                                        
+                    while (datos.Read()) //returing false 
+                    {
+                        try{
+                            //Tipo de muestras
+                            string[] tipoMuestra = new string[] { "Normal", "Vapo 2,5ml", "No Vapo 2,5ml", "Cabello", "Tester", "No Vapo 1 ml.", "Cabello 20ml", "Cabello 30ml","No Vapo 2,5ml" };
+                            //Preparamos la ubicación en valores correctos. 
+                            string valormuestra = "";
+                            if (datos["B_Muestra"].ToString().Length == 1)
+                            {
+                                 valormuestra = "900" + datos["B_Muestra"].ToString();                                
+                            }
+
+                            else if (datos["B_Muestra"].ToString().Length == 2)
+                            {                                
+                                 valormuestra = "90" + datos["B_Muestra"].ToString(); 
+                            }
+                            else if (datos["B_Muestra"].ToString().Length == 3)
+                            {
+                                 valormuestra = "9" + datos["B_Muestra"].ToString();                               
+                            }
+                            //Añadimos una primera fila al ListView
+                            elementosFila[0] = datos["CodigoArticulo"].ToString();
+                            elementosFila[1] = datos["descripcion"].ToString();
+                            elementosFila[2] = datos["CodigoAlternativo"].ToString();                            
+                            elementosFila[3] = tipoMuestra[int.Parse(datos["B_EsVapo"].ToString())];
+                            elementosFila[4] = valormuestra;
+                            elementoListView = new ListViewItem(elementosFila);
+                            listaResultados.Items.Add(elementoListView);
+
+                            sageID = datos["CodigoArticulo"].ToString();
+                            sagelb.Text = datos["CodigoArticulo"].ToString();
+                            muestraID = valormuestra;
+                            }
+                        catch
+                        {
+                            MessageBox.Show("List Fails Load");
+                        }
+                    }                                                        
+                } else { MessageBox.Show("SQL KO producto"); }
+                    
+                
+                conectorSQL.Close();
+            }
+            catch
+            { 
+                MessageBox.Show("FAILS SQL ");
+            }
+        }
+
+
+        private void productoUbicado()
+        {
+            try
+            {
+                MessageBox.Show(sageID);
+                //BOOLEANO PARA REALIZAR INSERT.
+                bool insertadoJose_articuloMarcaContador = true;
+                string codigomarca = "366";
+                SqlConnection conectorSQL = new SqlConnection("user id=sa;password=Admin123admin;server=SRVSQL\\LOGICCLASS;database=lc_perfumesvalencia;connection timeout=30");
+                string consultaArticulosMarcaContador = "SELECT     CodigoArticulo FROM   jose_ArticulosMarcaContador WHERE     (Valor = '" + muestraID + "') AND (B_CodigoMarca = '" + codigomarca + "') AND (CodigoArticulo = '" + sageID + "')";
+                string consultaUbicaciones = "SELECT     CodigoEmpresa, CodigoAlmacen, Ubicacion, DescripcionUbicacion  FROM  Ubicaciones  WHERE     (Ubicacion ='" + sagelb.Text + "')";
+                //PRIMERA CONEXIÓN SQL. 
+                conectorSQL.Open();                                                
+                SqlCommand comando = new SqlCommand(consultaArticulosMarcaContador, conectorSQL);                
+                SqlDataReader datosArtMarcaCont = comando.ExecuteReader();
+               
+                //Definimos array básico para tipo de muestra.                
+                if (!datosArtMarcaCont.HasRows) { MessageBox.Show("HACEMOS INSERT DEFINITIVO."); insertadoJose_articuloMarcaContador = false; }
+                
+                conectorSQL.Close();
+                
+                
+                //SqlCommand comando2 = new SqlCommand(consultaUbicaciones, conectorSQL);
+                //SqlDataReader datosUbicaciones = comando2.ExecuteReader();
+                //if (datosUbicaciones.HasRows)
+                //{
+                //    MessageBox.Show("ENTRA SEGUNDO IF");
+                //}
+                //else { MessageBox.Show("NO EXISTE PRODUCTO, HACEMOS INSERT."); }
+
+                //conectorSQL.Close();
+                btn_aplicar.Enabled = false;
+            }
+            catch
+            {
+                MessageBox.Show("FALLO EN EL INSERT2 ");
+            }
+
+        }
+       
+    }
+}
